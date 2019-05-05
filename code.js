309094c336f93d493ede9dce36c5ba2522503e0c
Rina <edsergeeva_1@edu.hse.ru>
2019-05-05 14:18:49 +0300

    manchester code sender
    Flirkering web page

---
+++ code.js
@@ -0,0 +1,111 @@
+function intToBinary(id, len) {
+    binary = [];
+    for (let i = 0; i < len; i++) {
+        binary.push(id % 2);
+        id >>= 1;
+    }
+    return binary.reverse();
+}
+
+function getMatrixHemming(width, height) {
+    let matrix = [];
+
+    for (let i = 0; i < width; i++) {
+        matrix.push([]);
+        let t = i + 1;
+        for (let j = 0; j < height; j++) {
+            matrix[i].push(t % 2);
+            t >>= 1;
+        }
+    }
+
+    matrix = matrix[0].map((col, i) => matrix.map(row => row[i]));
+    return matrix;
+}
+
+function encodeHemming(binary) {
+    let extendedArr = [];
+    let baseArrIndex = 0;
+    let twoPow = 1;
+    let powsIndex = [];
+    while (baseArrIndex < binary.length) {
+        if (extendedArr.length + 1 == twoPow) {
+            powsIndex.push(extendedArr.length);
+            extendedArr.push(0);
+            twoPow *= 2;
+        } else {
+            extendedArr.push(binary[baseArrIndex]);
+            baseArrIndex++;
+        }
+    }
+
+    let matrix = getMatrixHemming(extendedArr.length, powsIndex.length)
+    let powsNum = []
+
+    for (let i = 0; i < powsIndex.length; i++) {
+        let sum = 0;
+        for (let j = 0; j < extendedArr.length; j++) {
+            sum += extendedArr[j] & matrix[i][j];
+        }
+        powsNum.push(sum % 2);
+    }
+
+    for (let i = 0; i < powsIndex.length; i++) {
+        extendedArr[powsIndex[i]] = powsNum[i];
+    }
+
+    return extendedArr;
+}
+
+function addPreambule(binary) {
+    let preambule = [1, 0, 1, 0, 1, 0, 1, 0];
+    return preambule.concat(binary);
+}
+
+let currentState = 0
+let tact = 2000
+let i = 0
+
+function setInitState() {
+    if (i == code.length)
+        i = 0;
+
+    if (currentState == code[i]) {
+        if (code[i] === 0) {
+            changeBackground(1);
+            currentState = 1;
+        }
+        else {
+            changeBackground(0)
+            currentState = 0;
+        }
+    }
+
+    setTimeout(setState, tact / 2);
+}
+
+function setState() {
+    if (i == code.length)
+        i = 0;
+
+    changeBackground(code[i]);
+    setTimeout(setInitState, tact / 2);
+
+    currentState = code[i];
+    i++;
+}
+
+function changeBackground(state) {
+    if (state === 0) {
+        document.getElementsByTagName("body")[0].style.backgroundColor = "black";
+    } else {
+        document.getElementsByTagName("body")[0].style.backgroundColor = "white";
+    }
+}
+
+const ID = 13;
+let code = intToBinary(ID, 8)
+code = encodeHemming(code)
+code = addPreambule(code)
+
+setInitState()
